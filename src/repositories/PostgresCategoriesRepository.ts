794f1f09b4101c15f798d632a5996a1323b7dec5
Lucas Neves <lucas54neves@outlook.com>
2021-04-20 16:23:54 -0300

    add an example category repository

---
+++ src/repositories/PostgresCategoriesRepository.ts
@@ -0,0 +1,16 @@
+import { Category } from '../model/Category'
+import { CategoriesRepository, CategoryCreationRequest } from '../dtos'
+
+export class PostgresCategoriesRepository implements CategoriesRepository {
+  findByName(name: string): Category {
+    throw new Error('Method not implemented.')
+  }
+
+  list(): Category[] {
+    throw new Error('Method not implemented.')
+  }
+
+  create({ name, description }: CategoryCreationRequest): void {
+    throw new Error('Method not implemented.')
+  }
+}

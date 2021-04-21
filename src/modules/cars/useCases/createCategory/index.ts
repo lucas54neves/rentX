5cd456f0eb8a0259388b7b437a4b5b0f84d3490b
Lucas Neves <lucas54neves@outlook.com>
2021-04-21 16:09:08 -0300

    add controller for category creation

--- src/modules/cars/useCases/createCategory/index.ts
+++ src/modules/cars/useCases/createCategory/index.ts
@@ -1,1 +1,15 @@
-export * from './CategoryCreationUseCase'
+import { CategoriesRepository } from '@modules/cars/repositories'
+import { CategoryCreationController } from './CategoryCreationController'
+import { CategoryCreationUseCase } from './CategoryCreationUseCase'
+
+const categoriesRepository = new CategoriesRepository()
+
+const categoryCreationUseCase = new CategoryCreationUseCase(
+  categoriesRepository
+)
+
+const categoryCreationController = new CategoryCreationController(
+  categoryCreationUseCase
+)
+
+export { categoryCreationController }
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,23 +1,14 @@
 import { Request, Response, Router } from 'express'
+
 import { CategoriesRepository } from '../modules/cars/repositories'
-import { CategoryCreationUseCase } from '../modules/cars/useCases/createCategory'
+import { categoryCreationController } from '../modules/cars/useCases/createCategory'
 
 const categoriesRoutes = Router()
 
 const categoriesRepository = new CategoriesRepository()
 
 categoriesRoutes.post('/', (request: Request, response: Response) => {
-  const { name, description } = request.body
-
-  const categoryCreationService = new CategoryCreationUseCase(categoriesRepository)
-
-  try {
-    categoryCreationService.execute({ name, description })
-  } catch (error) {
-    return response.status(400).json({ message: error.message })
-  }
-
-  return response.status(201).send()
+  return categoryCreationController.handle(request, response)
 })
 
 categoriesRoutes.get('/', (request: Request, response: Response) => {

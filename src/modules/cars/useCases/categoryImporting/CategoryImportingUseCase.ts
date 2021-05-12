3f271a3ea377d585d9b16bbf852ac60bef28f611
Lucas Neves <lucas54neves@outlook.com>
2021-05-12 10:47:23 -0300

    add use case for category importing

---
+++ src/modules/cars/useCases/categoryImporting/CategoryImportingUseCase.ts
@@ -0,0 +1,56 @@
+import csvParse from 'csv-parse'
+import fs from 'fs'
+
+import { CategoryImportingRequest } from '../../dtos'
+import { ICategoriesRepository } from '../../repositories'
+
+export class CategoryImportingUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  loadCategories(
+    file: Express.Multer.File
+  ): Promise<CategoryImportingRequest[]> {
+    return new Promise((resolve, reject) => {
+      const categories: CategoryImportingRequest[] = []
+
+      const stream = fs.createReadStream(file.path)
+
+      const parseFile = csvParse()
+
+      stream.pipe(parseFile)
+
+      parseFile
+        .on('data', async (line) => {
+          const [name, description] = line
+
+          categories.push({
+            name,
+            description
+          })
+        })
+        .on('end', () => {
+          resolve(categories)
+        })
+        .on('error', (err) => {
+          reject(err)
+        })
+    })
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file)
+
+    categories.map((category) => {
+      const { name, description } = category
+
+      const existCategory = this.categoriesRepository.findByName(name)
+
+      if (!existCategory) {
+        this.categoriesRepository.create({
+          name,
+          description
+        })
+      }
+    })
+  }
+}

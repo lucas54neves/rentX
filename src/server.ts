f558afb6438b20850570cf410e28b966e2e6cc29
Lucas Neves <lucas54neves@outlook.com>
2021-05-13 18:29:07 -0300

    add database index

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import swaggerUi from 'swagger-ui-express'
 import swaggerFile from './swagger.json'
 import { routes } from './routes'
 
+import './database'
+
 const app = express()
 
 app.use(express.json())

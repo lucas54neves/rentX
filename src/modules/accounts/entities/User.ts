df22437ab117a937bb234ee938337702f748db8e
Lucas Neves <lucas54neves@outlook.com>
2021-05-19 19:01:24 -0300

    add user entity

---
+++ src/modules/accounts/entities/User.ts
@@ -0,0 +1,48 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm'
+import { v4 as uuidv4 } from 'uuid'
+
+@Entity('users')
+class User {
+  @PrimaryColumn()
+  id: string
+
+  @Column()
+  name: string
+
+  @Column()
+  username: string
+
+  @Column()
+  email: string
+
+  @Column()
+  password: string
+
+  @Column()
+  driverLicense: string
+
+  @Column()
+  isAdmin: boolean
+
+  @CreateDateColumn()
+  createdAt: Date
+
+  constructor(
+    name: string,
+    username: string,
+    email: string,
+    password: string,
+    driverLicense: string
+  ) {
+    this.id = uuidv4()
+    this.name = name
+    this.username = username
+    this.email = email
+    this.password = password
+    this.driverLicense = driverLicense
+    this.isAdmin = false
+    this.createdAt = new Date()
+  }
+}
+
+export { User }

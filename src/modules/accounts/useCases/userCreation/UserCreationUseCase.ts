637a113fccbf6b97f5dffdb52319409e3feef0fe
Lucas Neves <lucas54neves@outlook.com>
2021-05-24 17:22:22 -0300

    add password encryption

--- src/modules/accounts/useCases/userCreation/UserCreationUseCase.ts
+++ src/modules/accounts/useCases/userCreation/UserCreationUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from 'bcrypt'
 import { inject, injectable } from 'tsyringe'
 
 import { UserCreationRequest } from '../../dtos'
@@ -17,11 +18,27 @@ class UserCreationUseCase {
     password,
     driverLicense
   }: UserCreationRequest) {
+    const emailAlreadyInUse = await this.usersRepository.findByEmail(email)
+
+    if (emailAlreadyInUse) {
+      throw new Error('Email already in use')
+    }
+
+    const usernameAlreadyInUse = await this.usersRepository.findByUsername(
+      username
+    )
+
+    if (usernameAlreadyInUse) {
+      throw new Error('Username already in use')
+    }
+
+    const passwordHashed = await hash(password, 8)
+
     await this.usersRepository.create({
       name,
       username,
       email,
-      password,
+      password: passwordHashed,
       driverLicense
     })
   }

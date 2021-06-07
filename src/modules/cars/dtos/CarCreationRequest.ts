481315f71ef1f90ad17f4cabdb72ab98868e8c69
Lucas Neves <lucas54neves@gmail.com>
2021-06-07 15:30:56 -0300

    add type for car creation request

---
+++ src/modules/cars/dtos/CarCreationRequest.ts
@@ -0,0 +1,9 @@
+export type CarCreationRequest = {
+  name: string
+  description: string
+  dailyRate: number
+  licensePlate: string
+  fineAmount: number
+  brand: string
+  categoryId: string
+}
--- src/modules/cars/dtos/index.ts
+++ src/modules/cars/dtos/index.ts
@@ -1,3 +1,4 @@
 export { CategoryCreationRequest } from './CategoryCreationRequest'
 export { SpecificationCreationRequest } from './SpecificationCreationRequest'
 export { CategoryImportingRequest } from './CategoryImportingRequest'
+export { CarCreationRequest } from './CarCreationRequest'

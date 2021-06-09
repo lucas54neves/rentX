60069184930ee0960b9595d8732f25414e3361a2
Lucas Neves <lucas54neves@gmail.com>
2021-06-09 14:43:29 -0300

    rename ICarsRepository to CrsRepositoryInterface

--- src/modules/cars/repositories/CarsRepositoryInterface.ts
+++ src/modules/cars/repositories/CarsRepositoryInterface.ts
@@ -1,10 +1,11 @@
-import { CarCreationRequest } from '../dtos'
+import { CarCreationRequest, ListAvailableCarsRequest } from '../dtos'
 import { Car } from '../infra/typeorm/entities'
 
 interface CarsRepositoryInterface {
   create(data: CarCreationRequest): Promise<Car>
   save(car: Car): Promise<void>
   findByLincensePlate(licensePlate: string): Promise<Car | undefined>
+  findAvailable(data: ListAvailableCarsRequest): Promise<Car[]>
 }
 
 export { CarsRepositoryInterface }

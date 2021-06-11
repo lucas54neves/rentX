e688cc0ba8c7a7ec4ae00eeead8365794ac9fac4
Lucas Neves <lucas54neves@gmail.com>
2021-06-11 19:57:33 -0300

    update cars routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,25 +1,35 @@
 import { Router } from 'express'
 
-import { CarCreationController } from '@modules/cars/useCases/carCreation/CarCreationController'
+import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
 import {
   ensureAdmin,
   ensureAuthenticated
 } from '@shared/infra/http/middlewares'
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController'
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController'
 
 const carsRoutes = Router()
 
-const carCreationController = new CarCreationController()
+const createCarController = new CreateCarController()
 
 const listAvailableCarsController = new ListAvailableCarsController()
+
+const createCarSpecificationController = new CreateCarSpecificationController()
 
 carsRoutes.post(
   '/',
   ensureAuthenticated,
   ensureAdmin,
-  carCreationController.handle
+  createCarController.handle
 )
 
 carsRoutes.get('/available', listAvailableCarsController.handle)
+
+carsRoutes.post(
+  '/specifications/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
+)
 
 export { carsRoutes }

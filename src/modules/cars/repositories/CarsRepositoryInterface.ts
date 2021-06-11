23832b6572a96a0b9439f2cc38a894808c8d5c4c
Lucas Neves <lucas54neves@gmail.com>
2021-06-11 19:54:39 -0300

    update cars repository interface

--- src/modules/cars/repositories/CarsRepositoryInterface.ts
+++ src/modules/cars/repositories/CarsRepositoryInterface.ts
@@ -1,11 +1,21 @@
-import { CreateCarRequest, ListAvailableCarsRequest } from '../dtos'
-import { Car } from '../infra/typeorm/entities'
+import {
+  AddSpecificationRequest,
+  CreateCarRequest,
+  ListAvailableCarsRequest,
+  FindSpecificationInCarsRepositoryRequest
+} from '../dtos'
+import { Car, Specification } from '../infra/typeorm/entities'
 
 interface CarsRepositoryInterface {
   create(data: CreateCarRequest): Promise<Car>
   save(car: Car): Promise<void>
+  findById(id: string): Promise<Car | undefined>
   findByLincensePlate(licensePlate: string): Promise<Car | undefined>
   findAvailable(data: ListAvailableCarsRequest): Promise<Car[]>
+  findSpecification(
+    data: FindSpecificationInCarsRepositoryRequest
+  ): Promise<Specification | undefined | null>
+  addSpecification(data: AddSpecificationRequest): Promise<void>
 }
 
 export { CarsRepositoryInterface }

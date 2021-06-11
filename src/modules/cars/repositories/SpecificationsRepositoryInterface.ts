adcbf01662ef1bd524062dad8e0eb523d24f8105
Lucas Neves <lucas54neves@gmail.com>
2021-06-11 19:55:49 -0300

    update specifications repository interface

--- src/modules/cars/repositories/SpecificationsRepositoryInterface.ts
+++ src/modules/cars/repositories/SpecificationsRepositoryInterface.ts
@@ -2,8 +2,12 @@ import { Specification } from '../infra/typeorm/entities'
 import { CreateSpecificationRequest } from '../dtos'
 
 export interface SpecificationsRepositoryInterface {
-  create({ name, description }: CreateSpecificationRequest): Promise<void>
+  create({
+    name,
+    description
+  }: CreateSpecificationRequest): Promise<Specification>
   save(specification: Specification): Promise<void>
   list(): Promise<Specification[]>
+  findByIds(ids: string[]): Promise<Specification[]>
   findByName(name: string): Promise<Specification | undefined>
 }

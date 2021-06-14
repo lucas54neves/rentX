924317c36b4f6f61b85619a87091133c7d12e917
Lucas <lucas54neves@gmail.com>
2021-06-14 20:30:15 -0300

    update cars routes

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import multer from 'multer'
 
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController'
 import {
@@ -7,14 +8,20 @@ import {
 } from '@shared/infra/http/middlewares'
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController'
 import { ListAvailableCarsController } from '@modules/cars/useCases/listAvailableCars/ListAvailableCarsController'
+import { UploadCarImageController } from '@modules/cars/useCases/uploadCarImages/UploadCarImagesController'
+import uploadConfig from '@config/upload'
 
 const carsRoutes = Router()
+
+const uploadImages = multer(uploadConfig.upload('./tmp/cars'))
 
 const createCarController = new CreateCarController()
 
 const listAvailableCarsController = new ListAvailableCarsController()
 
 const createCarSpecificationController = new CreateCarSpecificationController()
+
+const uploadCarImagesController = new UploadCarImageController()
 
 carsRoutes.post(
   '/',
@@ -30,6 +37,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+)
+
+carsRoutes.post(
+  '/images/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadImages.array('images'),
+  uploadCarImagesController.handle
 )
 
 export { carsRoutes }

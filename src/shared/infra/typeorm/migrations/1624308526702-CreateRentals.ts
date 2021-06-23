220f3a6e937aed6d33cd686231ebf5a8ad0b2c55
Lucas Neves <lucas54neves@gmail.com>
2021-06-23 17:46:17 -0300

    add migration to create rental table

---
+++ src/shared/infra/typeorm/migrations/1624308526702-CreateRentals.ts
@@ -0,0 +1,77 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateRentals1624308526702 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'rentals',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'carId',
+            type: 'uuid'
+          },
+          {
+            name: 'userId',
+            type: 'uuid'
+          },
+          {
+            name: 'startDate',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'endDate',
+            type: 'timestamp',
+            isNullable: true
+          },
+          {
+            name: 'expectedReturnDate',
+            type: 'timestamp'
+          },
+          {
+            name: 'total',
+            type: 'numeric',
+            isNullable: true
+          },
+          {
+            name: 'createdAt',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'updatedAt',
+            type: 'timestamp',
+            default: 'now()'
+          }
+        ],
+        foreignKeys: [
+          {
+            name: 'FKCarRental',
+            referencedTableName: 'cars',
+            referencedColumnNames: ['id'],
+            columnNames: ['carId'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL'
+          },
+          {
+            name: 'FKUserRental',
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            columnNames: ['userId'],
+            onDelete: 'SET NULL',
+            onUpdate: 'SET NULL'
+          }
+        ]
+      })
+    )
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('rentals')
+  }
+}

b9bc2730074cc877c3daae887408f36c109d53ef
Lucas Neves <lucas54neves@gmail.com>
2021-06-27 12:20:55 -0300

    add file to app

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,39 @@
+import express, { NextFunction, Request, Response } from 'express'
+import 'express-async-errors'
+import swaggerUi from 'swagger-ui-express'
+
+import createConnection from '@shared/infra/typeorm'
+import '@shared/container'
+import swaggerFile from '../../../swagger.json'
+import { routes } from '@shared/infra/http/routes'
+import { AppError } from '@shared/errors'
+
+createConnection()
+
+const app = express()
+
+app.disable('x-powered-by')
+
+app.use(express.json())
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
+
+app.use(routes)
+
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        status: 'app-error',
+        message: err.message
+      })
+    } else {
+      return response.status(500).json({
+        status: 'server-error',
+        message: `Internal server error - ${err.message}`
+      })
+    }
+  }
+)
+
+export { app }

aac02696a60a87f594ef382e58698b2bc665950f
Lucas Neves <lucas54neves@gmail.com>
2021-07-13 22:47:03 -0300

    add auth config

---
+++ src/config/auth.ts
@@ -0,0 +1,11 @@
+export default {
+  token: {
+    secretKey: '997db1d173c7d429f21aa54fe4faaad6',
+    expiresIn: '15m'
+  },
+  refreshToken: {
+    secretKey: '5ee0ca72bd9cf649f8989dc076891069',
+    expiresIn: '30d',
+    expiresInDays: 30
+  }
+}
